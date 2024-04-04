959fdfad977c7e87706594257a40565e39ee021d
AbdullohDev07 <gamesupro01@gmail.com>
2024-04-04 12:23:59 +0500

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,373 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { inject, TemplateRef, ViewEncapsulation } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { ViewChild } from '@angular/core';
+import { NgbCarousel, NgbCarouselModule, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCalendar, NgbDatepickerModule, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { JsonPipe } from '@angular/common';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbScrollSpyModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterLink } from '@angular/router';
+import { DecimalPipe } from '@angular/common';
+import { NgbTimepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbTypeahead, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+import { Observable, Subject, merge, OperatorFunction } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map } from 'rxjs/operators';
+
+const FILTER_PAG_REGEX = /[^0-9]/g;
+
+interface Alert {
+	type: string;
+	message: string;
+}
+
+const states = [
+	'Alabama',
+	'Alaska',
+	'American Samoa',
+	'Arizona',
+	'Arkansas',
+	'California',
+	'Colorado',
+	'Connecticut',
+	'Delaware',
+	'District Of Columbia',
+	'Federated States Of Micronesia',
+	'Florida',
+	'Georgia',
+	'Guam',
+	'Hawaii',
+	'Idaho',
+	'Illinois',
+	'Indiana',
+	'Iowa',
+	'Kansas',
+	'Kentucky',
+	'Louisiana',
+	'Maine',
+	'Marshall Islands',
+	'Maryland',
+	'Massachusetts',
+	'Michigan',
+	'Minnesota',
+	'Mississippi',
+	'Missouri',
+	'Montana',
+	'Nebraska',
+	'Nevada',
+	'New Hampshire',
+	'New Jersey',
+	'New Mexico',
+	'New York',
+	'North Carolina',
+	'North Dakota',
+	'Northern Mariana Islands',
+	'Ohio',
+	'Oklahoma',
+	'Oregon',
+	'Palau',
+	'Pennsylvania',
+	'Puerto Rico',
+	'Rhode Island',
+	'South Carolina',
+	'South Dakota',
+	'Tennessee',
+	'Texas',
+	'Utah',
+	'Vermont',
+	'Virgin Islands',
+	'Virginia',
+	'Washington',
+	'West Virginia',
+	'Wisconsin',
+	'Wyoming',
+];
+
+const ALERTS: Alert[] = [
+	{
+		type: 'success',
+		message: 'This is an success alert',
+	},
+	{
+		type: 'info',
+		message: 'This is an info alert',
+	},
+	{
+		type: 'warning',
+		message: 'This is a warning alert',
+	},
+	{
+		type: 'danger',
+		message: 'This is a danger alert',
+	},
+	{
+		type: 'primary',
+		message: 'This is a primary alert',
+	},
+	{
+		type: 'secondary',
+		message: 'This is a secondary alert',
+	},
+	{
+		type: 'light',
+		message: 'This is a light alert',
+	},
+	{
+		type: 'dark',
+		message: 'This is a dark alert',
+	},
+];
+
+interface Country {
+	name: string;
+	flag: string;
+	area: number;
+	population: number;
+}
+
+const COUNTRIES: Country[] = [
+	{
+		name: 'Russia',
+		flag: 'f/f3/Flag_of_Russia.svg',
+		area: 17075200,
+		population: 146989754,
+	},
+	{
+		name: 'Canada',
+		flag: 'c/cf/Flag_of_Canada.svg',
+		area: 9976140,
+		population: 36624199,
+	},
+	{
+		name: 'United States',
+		flag: 'a/a4/Flag_of_the_United_States.svg',
+		area: 9629091,
+		population: 324459463,
+	},
+	{
+		name: 'China',
+		flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
+		area: 9596960,
+		population: 1409517397,
+	},
+];
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbAccordionModule, NgbAlertModule, NgbCarouselModule, FormsModule, NgbCollapseModule, NgbDatepickerModule, FormsModule, JsonPipe, NgbDropdownModule, NgbNavModule, NgbPaginationModule, NgbPopoverModule, NgbProgressbarModule, NgbRatingModule, NgbScrollSpyModule, FormsModule, RouterLink, DecimalPipe, NgbTimepickerModule, FormsModule, JsonPipe, NgbToastModule, NgbTooltipModule, NgbTypeaheadModule, FormsModule, JsonPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  encapsulation: ViewEncapsulation.None,
+	styles: `
+		.dark-modal .modal-content {
+			background-color: #292b2c;
+			color: white;
+		}
+		.dark-modal .close {
+			color: white;
+		}
+		.light-blue-backdrop {
+			background-color: #5cb3fd;
+		}
+    .form-control { width: 300px; }
+	`,
+  host: { class: 'd-block' },
 })
 export class AppComponent {
   title = 'OneProj';
+
+  alerts!: Alert[];
+
+	constructor() {
+		this.reset();
+	}
+
+	close(alert: Alert) {
+		this.alerts.splice(this.alerts.indexOf(alert), 1);
+	}
+
+  images = [62, 83, 466, 965, 982, 1043, 738].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+	paused = false;
+	unpauseOnArrow = false;
+	pauseOnIndicator = false;
+	pauseOnHover = true;
+	pauseOnFocus = true;
+
+	@ViewChild('carousel', { static: true }) carousel!: NgbCarousel;
+
+	togglePaused() {
+		if (this.paused) {
+			this.carousel.cycle();
+		} else {
+			this.carousel.pause();
+		}
+		this.paused = !this.paused;
+	}
+
+	onSlide(slideEvent: NgbSlideEvent) {
+		if (
+			this.unpauseOnArrow &&
+			slideEvent.paused &&
+			(slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
+		) {
+			this.togglePaused();
+		}
+		if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+			this.togglePaused();
+		}
+	}
+
+  isCollapsed = false;
+
+  today = inject(NgbCalendar).getToday();
+
+	model!: NgbDateStruct;
+	date!: { year: number; month: number };
+
+  active = 1;
+
+
+  private offcanvasService = inject(NgbOffcanvas);
+
+	openEnd(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { position: 'end' });
+	}
+
+	openTop(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { position: 'top' });
+	}
+
+	openBottom(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { position: 'bottom' });
+	}
+
+	openNoBackdrop(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { backdrop: false });
+	}
+
+	openStaticBackdrop(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { backdrop: 'static' });
+	}
+
+	openScroll(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { scroll: true });
+	}
+
+	openNoKeyboard(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { keyboard: false });
+	}
+
+	openNoAnimation(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { animation: false });
+	}
+
+	openCustomBackdropClass(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { backdropClass: 'bg-info' });
+	}
+
+	openCustomPanelClass(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { panelClass: 'bg-info' });
+	}
+
+  page = 4;
+
+	getPageSymbol(current: number) {
+		return ['A', 'B', 'C', 'D', 'E', 'F', 'G'][current - 1];
+	}
+
+	selectPage(page: string) {
+		this.page = parseInt(page, 10) || 1;
+	}
+
+	formatInput(input: HTMLInputElement) {
+		input.value = input.value.replace(FILTER_PAG_REGEX, '');
+	}
+
+  selected = 0;
+	hovered = 0;
+	readonly = false;
+
+  countries = COUNTRIES;
+
+	reset() {
+		this.alerts = Array.from(ALERTS);
+	}
+
+  private modalService = inject(NgbModal);
+
+	openBackDropCustomClass(content: TemplateRef<any>) {
+		this.modalService.open(content, { backdropClass: 'light-blue-backdrop' });
+	}
+
+	openWindowCustomClass(content: TemplateRef<any>) {
+		this.modalService.open(content, { windowClass: 'dark-modal' });
+	}
+
+	openSm(content: TemplateRef<any>) {
+		this.modalService.open(content, { size: 'sm' });
+	}
+
+	openLg(content: TemplateRef<any>) {
+		this.modalService.open(content, { size: 'lg' });
+	}
+
+	openXl(content: TemplateRef<any>) {
+		this.modalService.open(content, { size: 'xl' });
+	}
+
+	openFullscreen(content: TemplateRef<any>) {
+		this.modalService.open(content, { fullscreen: true });
+	}
+
+	openVerticallyCentered(content: TemplateRef<any>) {
+		this.modalService.open(content, { centered: true });
+	}
+
+	openScrollableContent(longContent: any) {
+		this.modalService.open(longContent, { scrollable: true });
+	}
+
+	openModalDialogCustomClass(content: TemplateRef<any>) {
+		this.modalService.open(content, { modalDialogClass: 'dark-modal' });
+	}
+
+  time = { hour: 13, minute: 30 };
+	meridian = true;
+
+  show = true;
+
+
+	@ViewChild('instance', { static: true }) instance!: NgbTypeahead;
+
+	focus$ = new Subject<string>();
+	click$ = new Subject<string>();
+
+	search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) => {
+		const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
+		const clicksWithClosedPopup$ = this.click$.pipe(filter(() => !this.instance.isPopupOpen()));
+		const inputFocus$ = this.focus$;
+
+		return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+			map((term) =>
+				(term === '' ? states : states.filter((v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10),
+			),
+		);
+	};
+
 }
